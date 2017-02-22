09d6d27c78e268ad01cd00d8c74f00d3a9eb2cf3
faimin <fuxianchao@gmail.com>
2017-02-22 23:57:43 +0800

    first commit

---
+++ CarthageTest/CarthageTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarthageTest
+//
+//  Created by 符现超 on 2017/2/22.
+//  Copyright © 2017年 Zero.D.Saber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarthageTest/CarthageTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarthageTest
+//
+//  Created by 符现超 on 2017/2/22.
+//  Copyright © 2017年 Zero.D.Saber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
