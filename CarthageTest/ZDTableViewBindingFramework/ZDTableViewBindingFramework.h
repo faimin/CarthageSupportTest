c59c8eb4e919563eb5dea324ca76fb9f400f7426
faimin <fuxianchao@gmail.com>
2017-02-24 00:28:41 +0800

    配置好文件

---
+++ CarthageTest/ZDTableViewBindingFramework/ZDTableViewBindingFramework.h
@@ -0,0 +1,19 @@
+//
+//  ZDTableViewBindingFramework.h
+//  ZDTableViewBindingFramework
+//
+//  Created by 符现超 on 2017/2/24.
+//  Copyright © 2017年 Zero.D.Saber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZDTableViewBindingFramework.
+FOUNDATION_EXPORT double ZDTableViewBindingFrameworkVersionNumber;
+
+//! Project version string for ZDTableViewBindingFramework.
+FOUNDATION_EXPORT const unsigned char ZDTableViewBindingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZDTableViewBindingFramework/PublicHeader.h>
+
+
